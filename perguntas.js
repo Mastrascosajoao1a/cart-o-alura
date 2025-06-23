c79c5289a3510761432fae3193ab824b3110e7d5
Mastrascosajoao1a <167330372+Mastrascosajoao1a@users.noreply.github.com>
2025-06-23 11:21:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Matemática',
+    '435 + 278?',
+    '713'
+)
+
+criaCartao(
+    'Português',
+    'Qual acento tem na palavra você?',
+    'Acento circunflexo'
+)
+
+criaCartao(
+    'Matématica',
+    '20 x 15',
+    '350'
+)
